a2611750b1c058d73078ba669cc23f77d510101f
OliveChao <chaoolive@gmail.com>
2014-07-31 13:42:30 +0300

    first commit

---
+++ TakaApp/gen/com/example/takaapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.takaapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TakaApp/src/com/example/takaapp/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.takaapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+
+		
+	}
+
+}
